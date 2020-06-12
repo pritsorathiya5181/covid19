173dbea3ada342956b6784dc20807ec4be8ad511
prit <prit.sorathiya@gmail.com>
2020-06-12 15:19:51 +0530

    "complete initial home screen"

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: kBackgroundColor,
         fontFamily: "Poppins",
         textTheme: TextTheme(
-          bodyText2 : TextStyle(color: kBodyTextColor),
+          bodyText2: TextStyle(color: kBodyTextColor),
         ),
       ),
       home: HomeScreen(),
@@ -30,12 +30,12 @@ class HomeScreen extends StatelessWidget {
       body: Column(
         children: <Widget>[
           ClipPath(
-              clipper: MyClipper(),
-              child: Container(
-                padding: EdgeInsets.only(left: 40, top: 50, right: 20),
-                height: 350,
-                width: double.infinity,
-                decoration: BoxDecoration(
+            clipper: MyClipper(),
+            child: Container(
+              padding: EdgeInsets.only(left: 40, top: 50, right: 20),
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
                   gradient: LinearGradient(
                     begin: Alignment.topRight,
                     end: Alignment.bottomLeft,
@@ -44,20 +44,18 @@ class HomeScreen extends StatelessWidget {
                       Color(0xFF11249F),
                     ],
                   ),
-                image: DecorationImage(
-                  image: AssetImage('assets/images/virus.png'),
-                )
-              ),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/virus.png'),
+                  )),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Align(
-                    alignment: Alignment.topRight,
-                    child: SvgPicture.asset('assets/icons/menu.svg')
-                    ),
+                      alignment: Alignment.topRight,
+                      child: SvgPicture.asset('assets/icons/menu.svg')),
                   SizedBox(height: 20.0),
                   Expanded(
-                      child: Stack(
+                    child: Stack(
                       children: <Widget>[
                         SvgPicture.asset(
                           'assets/icons/Drcorona.svg',
@@ -66,14 +64,12 @@ class HomeScreen extends StatelessWidget {
                           alignment: Alignment.topCenter,
                         ),
                         Positioned(
-                          top: 30,
+                          top: 10,
                           left: 150,
-                          child: Text(
-                              'All you need \nis stay at home',
+                          child: Text('All you need \nis stay at home',
                               style: kHeadingTextStyle.copyWith(
                                 color: Colors.white,
-                              )
-                            ),
+                              )),
                         ),
                         Container(),
                       ],
@@ -89,16 +85,15 @@ class HomeScreen extends StatelessWidget {
             height: 60,
             width: double.infinity,
             decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(25),
-              border: Border.all(
-                color: Color(0xFFE5E5E5),
-              )
-            ),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25),
+                border: Border.all(
+                  color: Color(0xFFE5E5E5),
+                )),
             child: Row(
               children: <Widget>[
                 SvgPicture.asset('assets/icons/maps-and-flags.svg'),
-                SizedBox(width: 20.0),
+                // SizedBox(width: 20.0),
                 Expanded(
                   child: DropdownButton(
                     isExpanded: true,
@@ -106,12 +101,12 @@ class HomeScreen extends StatelessWidget {
                     icon: SvgPicture.asset('assets/icons/dropdown.svg'),
                     value: 'India',
                     items: ['India', 'Bangladesh', 'United States', 'japan']
-                      .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
                     onChanged: (value) {},
                   ),
                 )
@@ -130,9 +125,7 @@ class HomeScreen extends StatelessWidget {
                         children: [
                           TextSpan(
                             text: 'Case Update\n',
-                            style: TextStyle(
-                              color: kTitleTextColor
-                            ),
+                            style: TextStyle(color: kTitleTextColor),
                           ),
                           TextSpan(
                             text: 'Newest update March 28',
@@ -145,11 +138,78 @@ class HomeScreen extends StatelessWidget {
                     Text(
                       'See details',
                       style: TextStyle(
-                        color: kPrimaryColor,
-                        fontWeight: FontWeight.w600
-                      ),
+                          color: kPrimaryColor, fontWeight: FontWeight.w600),
                     ),
                   ],
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: kShadowColor,
+                        offset: Offset(0, 3),
+                        blurRadius: 30.0,
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Counter(
+                        number: 285342,
+                        title: "Infected",
+                        color: kInfectedColor,
+                      ),
+                      Counter(
+                        number: 4014,
+                        title: "Death",
+                        color: kDeathColor,
+                      ),
+                      Counter(
+                        number: 190673,
+                        title: "Recovered",
+                        color: kRecovercolor,
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      'Spread of virus',
+                      style: kTitleTextstyle,
+                    ),
+                    Spacer(),
+                    Text(
+                      'see details',
+                      style: TextStyle(
+                          color: kPrimaryColor, fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  height: 168,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 4),
+                        blurRadius: 30,
+                        color: kShadowColor,
+                      ),
+                    ],
+                  ),
+                  child: Image.asset(
+                    'assets/images/map.png',
+                    fit: BoxFit.contain,
+                  ),
                 )
               ],
             ),
@@ -160,12 +220,60 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
+class Counter extends StatelessWidget {
+  final int number;
+  final Color color;
+  final String title;
+
+  const Counter({
+    Key key,
+    this.number,
+    this.color,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(6.0),
+          width: 25,
+          height: 25,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color.withOpacity(.26),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.transparent,
+                border: Border.all(
+                  color: color,
+                  width: 2,
+                )),
+          ),
+        ),
+        Text(
+          '$number',
+          style: TextStyle(
+            fontSize: 30,
+            color: color,
+          ),
+        ),
+        Text(title, style: kSubTextStyle),
+      ],
+    );
+  }
+}
+
 class MyClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
     path.lineTo(0, size.height - 80);
-    path.quadraticBezierTo(size.width/2, size.height, size.width, size.height - 80);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 80);
     path.lineTo(size.width, 0);
     return path;
   }
@@ -174,5 +282,4 @@ class MyClipper extends CustomClipper<Path> {
   bool shouldReclip(CustomClipper<Path> oldClipper) {
     return null;
   }
-
 }
